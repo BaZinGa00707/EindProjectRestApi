8bfe2c4ec3f140d5a7e5e554f436b2455bd1f861
Unknown <inias--@hotmail.com>
2018-05-15 12:45:34 +0200

    code toegevoegd voor gebruiken Api

--- RESTfulapiProject/src/app/app.component.ts
+++ RESTfulapiProject/src/app/app.component.ts
@@ -1,10 +1,31 @@
-import { Component } from '@angular/core';
+import { Component , OnInit } from '@angular/core';
+import { PokemonServiceService } from "./pokemon-service.service";
+import { PokemonInterface } from "./pokemon-interface"
+
+
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
+  
   title = 'app';
+  _dataArray : PokemonInterface[];
+
+
+  constructor(private pokemonService: PokemonServiceService ){}
+
+  getPosts(): void {
+    this.pokemonService.getPosts()
+          .subscribe(
+            resultArray => this._dataArray = resultArray,
+            error => console.log("Error ::" + error)
+          )
+  }
+
+  ngOnInit(): void {
+    this.getPosts();
+  }
 }
