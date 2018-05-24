9405f32659667a64d4f80452d43a4e9ea8254dae
Inias Somers <33604781+BaZinGa00707@users.noreply.github.com>
2018-05-24 09:32:29 +0200

    Api begin + testen

---
+++ ApiProject/ApiProject/Controllers/TestController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ApiProject.Controllers
+{
+    [Route("api/test")]
+    public class TestController
+    {
+        // GET api/values
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "test Controllerr", "" };
+        }
+
+        // GET api/values/5
+        [HttpGet("{check}")]
+        public string Get(int id)
+        {
+            return "check";
+        }
+
+    }
+}
